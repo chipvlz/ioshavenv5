6f43450c3489de707bcf79187b61eb76524bb91e
Zebthewizard <burroughszeb@me.com>
2018-04-15 11:47:19 -0500

    First commit

---
+++ resources/assets/js/app.js
@@ -0,0 +1,52 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+Vue.component('example-component', require('./components/ExampleComponent.vue'));
+
+const app = new Vue({
+    el: '#app',
+    data: {
+      showmoreclick: false,
+      scrollpos: 0,
+      hasScrolledOnePage: false
+    },
+    mounted () {
+      this.scrollpos = window.pageYOffset || document.documentElement.scrollTop;
+      this.hasScrolledOnePage = this.scrollpos > window.innerHeight;
+      window.addEventListener('scroll', (e) => {
+        this.scrollpos = window.pageYOffset || document.documentElement.scrollTop;
+        this.hasScrolledOnePage = this.scrollpos > window.innerHeight;
+      });
+    }
+});
+
+$(".totop").on('click', function (e) {
+  e.preventDefault();
+  $("html, body").animate({scrollTop: 0}, 400);
+});
+
+$('.logout').on('click', function (e) {
+  e.preventDefault();
+  $('#logout').submit();
+})
+
+$('.locale').on('click', function (e) {
+  e.preventDefault();
+  $('#locale-value').val($(this).data('value'));
+  // console.log($('#locale').serialize());
+  $('#locale').submit();
+})
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{env("APP_TYPE")}} Haven</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600" rel="stylesheet">
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <div id="app">
+        @include('layouts.navigation')
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+
+    <script src="/js/app.js" charset="utf-8"></script>
+    <script src="/fa/svg-with-js/js/fontawesome-all.min.js" charset="utf-8"></script>
+</body>
+</html>
