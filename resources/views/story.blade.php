723f1ad2e1d3c39a8a503f0fdadcd3b2b4ab465b
criticalthewizard <burroughszeb@me.com>
2018-06-25 21:55:17 -0500

    more css

--- resources/views/story.blade.php
+++ resources/views/story.blade.php
@@ -22,14 +22,15 @@
 <div class="container story">
 
   <div class="row">
-    <div class="col-md-8 m-auto">
-      <div class="app-banner mb-3">
-        <img id="banner-image" src="{{ $published->image }}" class="w-100" style="max-width: 400px" alt="banner" v-pre>
-      </div>
-      <h1 class="pt-4" v-pre>{{$published->title}}</h1>
-      <h5 v-pre>{{ $published->mini }}</h5>
+    <div class="col-md-10 m-auto">
 
-      <div class="mt-5" v-pre>{!! $published->content !!}</div>
+      <h1 class="pt-4 display-4 mb-2" v-pre>{{$published->title}}</h1>
+      <h5 v-pre>{{ $published->mini }}</h5>
+      <hr>
+
+      <img id="banner-image" src="{{ $published->image }}" class="w-100 float-md-left mr-3 mb-3" style="max-width: 400px" alt="banner" v-pre>
+
+      <div class="col-md-12" v-pre>{!! $published->content !!}</div>
 
       <br>
       <hr>
